b6952a894c7b4dbd50d7f23ea48bcfe58f1a1bc7
grahamwobig <grahamwo@vt.edu>
2018-11-14 20:31:26 +0000

    implemented moving average

---
+++ Ultrasonics/example/moving_average.cpp
@@ -0,0 +1,33 @@
+#include "moving_average.h"
+
+/**
+ * Create a new moving average object in your code. The number in the 
+ * constructor is how many measurements this class will average over.
+ * 
+ * When the sensor gets a reading, add it with addMeasurement() and that 
+ * method will return the current average;
+ */
+
+using namespace std;
+
+Moving_Average::Moving_Average(unsigned int x) {
+	q = new queue<unsigned int>;
+	windowLength = x;
+	totalSum = 0;
+}
+
+Moving_Average::~Moving_Average() {
+	delete q;
+}
+
+double Moving_Average::addMeasurement(unsigned int measurement) {
+	q->push(measurement);
+	totalSum += measurement;
+	
+	if (q->size() > windowLength) {
+		totalSum -= q->front();
+		q->pop();
+	}
+	
+	return (double)totalSum / (double) q->size();
+}
