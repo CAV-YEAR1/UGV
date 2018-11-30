afaff1f94cfd85ad06e4d7df1629ed95ed129791
grahamwobig <grahamwo@vt.edu>
2018-11-30 17:32:02 +0000

    switch to terminal rc

---
+++ Follow_Vehicle/TerminalRC/build/CMakeFiles/TestCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCode.dir/keyPress.cpp.o"
+  "TestCode.pdb"
+  "TestCode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
