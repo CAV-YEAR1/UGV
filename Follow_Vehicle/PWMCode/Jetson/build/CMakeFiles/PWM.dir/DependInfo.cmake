37e46b08c9ff457dbb496a637946a3014e6ab12e
grahamwobig <grahamwo@vt.edu>
2018-12-01 16:33:43 +0000

    added build files to git ignore

---
+++ Follow_Vehicle/PWMCode/Jetson/build/CMakeFiles/PWM.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/PWM.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/build/CMakeFiles/PWM.dir/PWM.cpp.o"
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/PWMMain.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/build/CMakeFiles/PWM.dir/PWMMain.cpp.o"
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/I2C_Code/Jetson/I2C.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/build/CMakeFiles/PWM.dir/home/nvidia/Documents/UGV/Follow_Vehicle/I2C_Code/Jetson/I2C.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Follow_Vehicle/remotePWM/build/CMakeFiles/RemotePWM.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/I2C_Code/Jetson/I2C.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/remotePWM/build/CMakeFiles/RemotePWM.dir/home/nvidia/Documents/UGV/Follow_Vehicle/I2C_Code/Jetson/I2C.cpp.o"
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/PWM.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/remotePWM/build/CMakeFiles/RemotePWM.dir/home/nvidia/Documents/UGV/Follow_Vehicle/PWMCode/Jetson/PWM.cpp.o"
+  "/home/nvidia/Documents/UGV/Follow_Vehicle/remotePWM/remote.cpp" "/home/nvidia/Documents/UGV/Follow_Vehicle/remotePWM/build/CMakeFiles/RemotePWM.dir/remote.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
